6c41d94f3addcd8125e6a806580f262526b81741
eviao <eviao.dev@gmail.com>
2014-06-22 22:32:54 +0800

    initial gulp

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+
+var rimraf = require('rimraf');
+
+var paths = {
+  less: 'assets/less/*.less'
+};
+
+// Not all tasks need to use streams
+// A gulpfile is just another node program and you can use all packages available on npm
+gulp.task('clean', function(cb){
+  rimraf('assets/css', cb);
+});
+
+gulp.task('less',['clean'], function () {
+  gulp.src(paths.less)
+    .pipe(less())
+    .pipe(gulp.dest('assets/css'));
+});
+
+// Rerun the task when a file changes
+gulp.task('watch', function() {
+  gulp.watch(paths.less, ['less']);
+});
+
+// The default task (called when you run `gulp` from cli)
+gulp.task('default', ['less','watch']);
